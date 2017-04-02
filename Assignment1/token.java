19caab848af1cc1f5b9dd6bd7550c64f56b767d1
Keegan Ferrett <keegan.ferrett@gmail.com>
2017-04-02 21:40:02 +0200

    token Class was missing

---
+++ Assignment1/token.java
@@ -0,0 +1,17 @@
+class token {
+	private String input = "";
+	private String inputType = "";
+	private String tokenID = "";
+	public token next = null;
+
+	public token(String _input, String _inputType, String _tokenID) {
+		input = _input;
+		inputType = _inputType;
+		tokenID = _tokenID;
+	}
+
+	public String toString() {
+		String temp = "[" + input + "," + inputType + "," + tokenID + "]";
+		return temp;
+	}
+}
